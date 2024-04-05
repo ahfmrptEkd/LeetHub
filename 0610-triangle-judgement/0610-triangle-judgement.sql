b0937b2fa93d2923e4e1e3915572aedcdbc78ada
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2024-04-06 02:07:53 +0900

    Update 0610-triangle-judgement.sql

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -3,3 +3,8 @@ SELECT *, IF((((abs(x) + abs(y)) > abs(z)) And
             ((abs(x) + abs(z)) > abs(y)) And
             ((abs(y) + abs(z)) > abs(x))), "Yes", "No") AS triangle
 FROM Triangle;
+
+-- SELECT x,y,z,
+-- case WHEN (x+y) > z AND (x+z) > y AND (y+z) > x THEN 'Yes' ELSE 'No' end AS triangle
+-- FROM Triangle 
+-- 조건문을 이용한 방식
