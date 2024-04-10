74f9b2896398f24a9a8eaeb7e562d3f242213d45
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2024-04-10 14:35:08 +0900

    Time: 41 ms (28.02%), Space: 16.7 MB (80.62%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        import re # split 함수 대신 re.split으로 정규표현식으로 제거
+
+        c = re.split(r'\s+', s.strip()) # \s+는 하나 이상의 공백을 의미
+        print(c)
+
+        l = 0
+        r = len(c) - 1
+
+
+        while l < r:
+            c[l], c[r] = c[r], c[l]
+            l += 1
+            r -= 1
+        
+        result = ' '.join(c)
+        print(' '.join(c))
+        return result
