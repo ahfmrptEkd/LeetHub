bb3e1336b11cb54b0d47a4bef9a698bb73b26ca3
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2024-09-19 20:20:12 +0900

    Time: 130 ms (10.81%), Space: 18.1 MB (8.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,7 +3,6 @@ class Solution:
         l, r = 0, 1
 
         for i in range(len(nums)-1):
-            # print(nums[l], nums[r-1])
 
             if r >= len(nums) or (nums[l] == nums[r]):   # 범위 제한
                 nums.pop(r)
