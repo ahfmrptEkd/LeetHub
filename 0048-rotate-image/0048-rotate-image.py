8fb89d2a99f138fe1b0a71b83385403266be846e
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2024-10-25 20:47:38 +0900

    Time: 0 ms (100%), Space: 16.8 MB (5.27%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,14 +1,10 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        n = len(matrix)
-        # 모든 원소를 temp 배열에 저장
-        temp = []
-        for i in range(n):
-            for j in range(n):
-                temp.append(matrix[i][j])
-        
-        # matrix[i][j] = matrix[n-1-j][i] 공식을 사용해서 한번에 변환
-        for i in range(n):
-            for j in range(n):
-                idx = (n-1-j)*n + i  # temp 배열에서의 위치 계산
-                matrix[i][j] = temp[idx]
+        n = len(matrix[0])
+        for i in range(n // 2):  # 테두리 개수  
+            for j in range(i, n-1-i):  # 현재 테두리에서 회전 가능한 범위
+                tmp = matrix[n-1-j][i]
+                matrix[n-1-j][i] = matrix[n-1-i][n-j-1]
+                matrix[n-1-i][n-j-1] = matrix[j][n-1-i]
+                matrix[j][n-1-i] = matrix[i][j]
+                matrix[i][j] = tmp
