0442d37335b2fb58b669bde40b679cf2f268c261
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2024-10-30 21:24:36 +0900

    Time: 7 ms (91.53%), Space: 16.7 MB (95.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,14 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict_s = Counter(s)
-        dict_t = Counter(t)
-
         # k 가 없으면 false
         # k 의 개수가 달라도 false
+        # 둘의 길이가 달라도 false
+        if len(s) != len(t):
+            return False
+
+        dict_s = Counter(s)
+        dict_t = Counter(t)
 
         for k in dict_s:
             if k not in dict_t:
