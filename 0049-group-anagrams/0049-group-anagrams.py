9dca7a71efa19b09547619d9530aa0189b002c54
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2024-11-01 21:53:56 +0900

    Time: 11 ms (80.23%), Space: 19.6 MB (80.1%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,17 +4,22 @@ class Solution:
         # 이차원 배열로 답을 넣는다.
         # {0:{b:1,a:1,t:1}} 이런식으로 찾고 해당하면
         # 0이면 [[0],[],[]]에 append 하면 될 듯 함
-        # n^2 임?
         # 정렬을 하면 abcd 처럼?
 
+        # 아나그램 그룹을 저장할 딕셔너리 초기화
         dict_word = {}
 
         for word in strs:
+            # 현재 단어의 문자들을 정렬하여 키로 사용
+            # 예: 'eat' -> 'aet', 'tea' -> 'aet'
             sorted_word = ''.join(sorted(word))
 
+            # 정렬된 문자열이 이미 딕셔너리에 있는 경우, 해당 키의 리스트에 현재 단어를 추가
             if sorted_word in dict_word:
                 dict_word[sorted_word].append(word)
             else:
+                # 새로운 키를 만들고 현재 단어를 포함하는 리스트를 값으로 저장
                 dict_word[sorted_word] = [word]
+
         result = [value for value in dict_word.values()]
         return result
