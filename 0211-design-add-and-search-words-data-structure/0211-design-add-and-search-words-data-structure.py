1792ec9a6b67380c8eb0ed2deefce753394b859d
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2025-01-16 22:38:09 +0900

    Time: 1341 ms (41.22%), Space: 65.4 MB (52.36%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,37 +1,33 @@
 class WordDictionary:
     def __init__(self):
-        self.children = {}  
-        self.is_end = False 
+        self.children = {}
+        self.is_end = False
 
     def addWord(self, word: str) -> None:
         curr = self
-        for c in word:  # 단어의 각 문자에 대해
-            if c not in curr.children:  # 현재 문자에 대한 노드가 없으면
-                curr.children[c] = WordDictionary()  # 새 노드 생성
-            curr = curr.children[c]  # 다음 노드로 이동
-        curr.is_end = True  # 단어의 끝 표시
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = WordDictionary()
+            curr = curr.children[c]
+        curr.is_end = True
 
-    # 단어를 검색하는 메서드 (와일드카드 '.' 지원)
     def search(self, word: str) -> bool:
-        def dfs(curr, i):
-            if i == len(word):  # 단어의 끝에 도달
-                return curr.is_end
-            
-            if word[i] == ".":  # 와일드카드 문자인 경우
-                for child in curr.children.values():  # 모든 자식 노드 탐색
-                    if dfs(child, i+1):  # 재귀적으로 다음 문자 검색
-                        return True
-                return False
-            
-            if word[i] not in curr.children:  # 현재 문자에 대한 노드가 없음
-                return False
-            
-            child = curr.children[word[i]]  # 다음 노드로 이동
-            return dfs(child, i+1)  # 재귀적으로 다음 문자 검색
+        stack = [(self, 0)]  # (노드, 현재 인덱스) 튜플을 저장하는 스택
         
-        return dfs(self, 0)  # 루트 노드에서 검색 시작
-
-# Your WordDictionary object will be instantiated and called as such:
-# obj = WordDictionary()
-# obj.addWord(word)
-# param_2 = obj.search(word)
+        while stack:
+            curr, i = stack.pop()
+            
+            if i == len(word):  # 단어의 끝에 도달
+                if curr.is_end:
+                    return True
+                continue
+                
+            if word[i] == ".":  # 와일드카드 문자인 경우
+                for child in curr.children.values():
+                    stack.append((child, i + 1))
+                    
+            else:  # 일반 문자인 경우
+                if word[i] in curr.children:
+                    stack.append((curr.children[word[i]], i + 1))
+                    
+        return False
