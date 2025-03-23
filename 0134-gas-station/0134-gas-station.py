d50cea21adf4c56e87b3064732775a82247f6bea
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2025-03-23 16:13:57 +0900

    Time :776 ms (5.08%)  & Space :22 MB (99.98%), Difficuly : Medium , Lang : .py

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,6 +1,9 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         # 한 바퀴를 돌 수 있는 수 가 있는지 아는게 중요
+            # a 에서 b 가는 사이 기름이 떨어진다면 그건 그 사이 값 어디에서 시작해도 도착 불가
+            # 한 바퀴를 돈다 그리고 무조건 돌 수 있는 조건이라면 답은 최소한개는 있다.
+            # 그 뒤 b에서 부탁 시작하면 된다.
 
         if sum(gas) < sum(cost):
             return -1
