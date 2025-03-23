0cc215b7018d53bfa33d9e7a725a2e10e86fc87e
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2025-03-23 17:55:47 +0900

    Medium ,03-23-2025

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, m * n - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            # 2d -> 1d 변환
+            # index/열의개수 = row(==몫) + (col/열의개수)(==나머지)
+            row = mid // n
+            col = mid % n
+
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return False
