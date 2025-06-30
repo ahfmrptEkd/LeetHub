ada5cace25e776d746bf2a27ea9716f8f21583be
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2025-06-30 14:37:16 +0900

    Medium ,06-30-2025

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -5,14 +5,17 @@ public:
         int slow = nums[0];
         int fast = nums[0];
         
-        do {
+        do 
+        {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while (slow != fast);
+        } 
+        while (slow != fast);
         
         // Phase 2: Find entrance to cycle
         slow = nums[0];
-        while (slow != fast) {
+        while (slow != fast)
+        {
             slow = nums[slow];
             fast = nums[fast];
         }
