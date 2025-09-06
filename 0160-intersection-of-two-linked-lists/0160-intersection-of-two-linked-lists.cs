34855b338b85671c7d0e13974fe246106d7ac0a8
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2025-09-07 01:12:33 +0900

    Easy ,09-07-2025

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode pointerA = headA;
+        ListNode pointerB = headB;
+
+        while (pointerA != pointerB)
+        {
+            pointerA = (pointerA == null) ? headB : pointerA.next;
+            pointerB = (pointerB == null) ? headA : pointerB.next;
+        }
+
+        return pointerA;
+    }
+}
