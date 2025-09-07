2d22d95b267fb9b5a940f6afede66661849f116c
Samuel Park <108009075+ahfmrptEkd@users.noreply.github.com>
2025-09-07 17:19:49 +0900

    Easy ,09-07-2025

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,19 +5,36 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        values = []
+        if not head or not head.next:  # ← 수정: not head.next
+            return True  # ← 수정: True 반환
+        
+        # 절반 점 찾기.
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second_half = self.reverse_half(slow.next)  
+
+        # 비교
+        first_half = head
+        
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half = first_half.next
+            second_half = second_half.next
+
+        return True
+
+    def reverse_half(self, head: Optional[ListNode]) -> Optional[ListNode]:  
+        prev = None
         curr = head
 
         while curr:
-            values.append(curr.val)
-            curr = curr.next
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
         
-        left, right = 0, len(values) - 1
-        
-        while left < right:
-            if values[left] != values[right]:
-                return False
-            left += 1
-            right -= 1
-        
-        return True
+        return prev
